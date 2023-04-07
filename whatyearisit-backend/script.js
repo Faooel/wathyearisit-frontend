4caedf3547d34a417b2de82986b4c81a5cb77485
Benoit <giannini.benoit@gmail.com>
2023-04-07 17:01:27 +0200

    debut repot front end

--- whatyearisit-backend/script.js
+++
@@ -1,5 +0,0 @@
-[33mcommit a8aeea1deed479c40a792e2a1d5f3125ab40a32a[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
-Author: Benoit <giannini.benoit@gmail.com>
-Date:   Fri Apr 7 16:31:03 2023 +0200
-
-    debut repot
