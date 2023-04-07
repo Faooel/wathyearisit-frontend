aaeb93656dd40b74e95cd4339b85945389327f45
Benoit <giannini.benoit@gmail.com>
2023-04-07 11:37:16 +0200

    debut nouveau commit exo

---
+++ whatyearisit-backend/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/date', (req, res) => {
+  const date = new Date();
+  res.json({ now: date });
+});
+
+module.exports = router;
