11a54659f81d210549ce4d93df08dff65b733f97
Benoit <giannini.benoit@gmail.com>
2023-04-07 17:23:00 +0200

    modif

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 fetch('https://wathyearisit-backend.vercel.app/')
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#year').textContent = data.now;
+        document.querySelector('#year').textContent = data.year;
         console.log(data)
     });
